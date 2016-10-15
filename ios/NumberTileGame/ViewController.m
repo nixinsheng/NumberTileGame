f2c2766b5932d2f207676faadd02897a2050ba43
枕头 <izhentou@163.com>
2016-10-15 23:30:07 +0800

    first

---
+++ ios/NumberTileGame/ViewController.m
@@ -0,0 +1,49 @@
+//
+//  ViewController.m
+//  NumberTileGame
+//
+//  Created by JackChen on 16/10/15.
+//  Copyright © 2016年 JackChen. All rights reserved.
+//
+
+#import "ViewController.h"
+#import "RCTRootView.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (IBAction)highScoreButtonPressed:(id)sender {
+    NSLog(@"High Score Button Pressed");
+    NSURL *jsCodeLocation = [NSURL
+                             URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios"];
+    RCTRootView *rootView =
+    [[RCTRootView alloc] initWithBundleURL : jsCodeLocation
+                         moduleName        : @"RNHighScores"
+                         initialProperties :
+     @{
+       @"scores" : @[
+               @{
+                   @"name" : @"Alex",
+                   @"value": @"42"
+                   },
+               @{
+                   @"name" : @"Joel",
+                   @"value": @"10"
+                   }
+               ]
+       }
+                          launchOptions    : nil];
+    UIViewController *vc = [[UIViewController alloc] init];
+    vc.view = rootView;
+    [self presentViewController:vc animated:YES completion:nil];
+}
+
+@end
---
+++ ios/NumberTileGameTests/NumberTileGameTests.m
@@ -0,0 +1,39 @@
+//
+//  NumberTileGameTests.m
+//  NumberTileGameTests
+//
+//  Created by JackChen on 16/10/15.
+//  Copyright © 2016年 JackChen. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface NumberTileGameTests : XCTestCase
+
+@end
+
+@implementation NumberTileGameTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
